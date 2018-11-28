1bf24319ef48a63dacd10464fa5dfee3447b135a
InvenireAudeAdmin <albert.krzymowski@invenireaude.com>
2018-11-28 17:59:39 +0100

    Extension interface changed.

--- IAS-LangMod-SysMon/src/lang/mod/monitor/sys/fs/GetUsage.h
+++ IAS-LangMod-SysMon/src/lang/mod/monitor/sys/fs/GetUsage.h
@@ -1,14 +1,14 @@
 /*
  * File: IAS-LangMod-SysMon/src/lang/mod/monitor/sys/fs/GetUsage.h
- * 
+ *
  * Copyright (C) 2015, Albert Krzymowski
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -41,10 +41,10 @@ public:
 
 
 	/** Creates an instance. */
-	static Statement* Create(const StringList& lstParamaters, const ::IAS::Lang::Interpreter::Extern::ModuleProxy* pModuleProxy);
+	static Statement* Create(const DM::Type* pType, const StringList& lstParamaters, const ::IAS::Lang::Interpreter::Extern::ModuleProxy* pModuleProxy);
 
 protected:
-	GetUsage(const StringList& lstParamaters, const ::IAS::Lang::Interpreter::Extern::ModuleProxy* pModuleProxy);
+	GetUsage(const DM::Type* pType, const StringList& lstParamaters, const ::IAS::Lang::Interpreter::Extern::ModuleProxy* pModuleProxy);
 
 	virtual void executeExternal(::IAS::Lang::Interpreter::Exe::Context *pCtx) const;
 
