77cda032da6eb548e84607e1c92a39bd19fdc194
InvenireAudeAdmin <albert.krzymowski@invenireaude.com>
2018-11-28 18:00:35 +0100

    Extension interface changed.

--- IAS-QSystemMod-SPS/src/qs/lang/sps/SetFallback.cpp
+++ IAS-QSystemMod-SPS/src/qs/lang/sps/SetFallback.cpp
@@ -1,14 +1,14 @@
 /*
  * File: IAS-QSystemMod-SPS/src/qs/lang/sps/SetFallback.cpp
- * 
+ *
  * Copyright (C) 2015, Albert Krzymowski
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -36,7 +36,7 @@ namespace Lang {
 namespace SPS {
 
 /*************************************************************************/
-SetFallback::SetFallback(const StringList& lstParamaters, const ::IAS::Lang::Interpreter::Extern::ModuleProxy* pModuleProxy){
+SetFallback::SetFallback(const DM::Type* pType, const StringList& lstParamaters, const ::IAS::Lang::Interpreter::Extern::ModuleProxy* pModuleProxy){
 	IAS_TRACER;
 }
 /*************************************************************************/
@@ -55,9 +55,9 @@ void SetFallback::executeExternal(Exe::Context *pCtx) const{
 	pProcessCacheEntry->setFallback(strActivity, tmScheduleOn);
 }
 /*************************************************************************/
-Extern::Statement* SetFallback::Create(const StringList& lstParamaters, const ::IAS::Lang::Interpreter::Extern::ModuleProxy* pModuleProxy){
+Extern::Statement* SetFallback::Create(const DM::Type* pType, const StringList& lstParamaters, const ::IAS::Lang::Interpreter::Extern::ModuleProxy* pModuleProxy){
 	IAS_TRACER;
-	return IAS_DFT_FACTORY<SetFallback>::Create(lstParamaters, pModuleProxy);
+	return IAS_DFT_FACTORY<SetFallback>::Create(pType, lstParamaters, pModuleProxy);
 }
 /*************************************************************************/
 }
--- IAS-QSystemMod-SPS/src/qs/lang/sps/Terminate.cpp
+++ IAS-QSystemMod-SPS/src/qs/lang/sps/Terminate.cpp
@@ -1,14 +1,14 @@
 /*
  * File: IAS-QSystemMod-SPS/src/qs/lang/sps/Terminate.cpp
- * 
+ *
  * Copyright (C) 2015, Albert Krzymowski
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -38,7 +38,7 @@ namespace Lang {
 namespace SPS {
 
 /*************************************************************************/
-Terminate::Terminate(const StringList& lstParamaters, const ::IAS::Lang::Interpreter::Extern::ModuleProxy* pModuleProxy){
+Terminate::Terminate(const DM::Type* pType, const StringList& lstParamaters, const ::IAS::Lang::Interpreter::Extern::ModuleProxy* pModuleProxy){
 	IAS_TRACER;
 }
 /*************************************************************************/
@@ -57,9 +57,9 @@ void Terminate::executeExternal(Exe::Context *pCtx) const{
 
 }
 /*************************************************************************/
-Extern::Statement* Terminate::Create(const StringList& lstParamaters, const ::IAS::Lang::Interpreter::Extern::ModuleProxy* pModuleProxy){
+Extern::Statement* Terminate::Create(const DM::Type* pType, const StringList& lstParamaters, const ::IAS::Lang::Interpreter::Extern::ModuleProxy* pModuleProxy){
 	IAS_TRACER;
-	return IAS_DFT_FACTORY<Terminate>::Create(lstParamaters, pModuleProxy);
+	return IAS_DFT_FACTORY<Terminate>::Create(pType, lstParamaters, pModuleProxy);
 }
 /*************************************************************************/
 }
