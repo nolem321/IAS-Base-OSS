d4cc3e0cf9422922fc2a4ed30504b7bc0a8abd30
InvenireAudeAdmin <albert.krzymowski@invenireaude.com>
2018-12-10 20:44:44 +0100

    More details on errors

--- IAS-CommonLib/src/commonlib/sys/DynamicLibrary.cpp
+++ IAS-CommonLib/src/commonlib/sys/DynamicLibrary.cpp
@@ -1,14 +1,14 @@
 /*
  * File: IAS-CommonLib/src/commonlib/sys/DynamicLibrary.cpp
- * 
+ *
  * Copyright (C) 2015, Albert Krzymowski
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -33,8 +33,11 @@ DynamicLibrary::DynamicLibrary(const String& strFileName, int  iMode):
 
 	IAS_LOG(IAS::LogLevel::INSTANCE.isSystem(),"["<<strFileName<<"]:"<<iMode);
 
-	if( (pHandle=dlopen(strFileName.c_str(), iMode)) == NULL)
-		IAS_THROW(IAS::Exception(dlerror()));
+	if( (pHandle=dlopen(strFileName.c_str(), iMode)) == NULL){
+    String strError(dlerror());
+	  IAS_LOG(IAS::LogLevel::INSTANCE.isError(),strError);
+  	IAS_THROW(IAS::Exception(strError));
+  }
 
 	IAS_LOG(IAS::LogLevel::INSTANCE.isSystem(),(void*)pHandle);
 }
