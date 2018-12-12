2fb68af4b453d9e625b0f7f6d59d10ea1cf94785
InvenireAudeAdmin <albert.krzymowski@invenireaude.com>
2018-12-12 21:09:42 +0100

    PureJSON formatter cleanup.

--- IAS-QSystemLib/src/qs/fmt/JSONPureFormatter.cpp
+++ IAS-QSystemLib/src/qs/fmt/JSONPureFormatter.cpp
@@ -1,14 +1,14 @@
 /*
  * File: IAS-QSystemLib/src/qs/fmt/JSONPureFormatter.cpp
- * 
+ *
  * Copyright (C) 2015, Albert Krzymowski
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -93,12 +93,8 @@ void JSONPureFormatter::read( DM::DataObjectPtr& dmData,
 		String strElementNS(strElement.substr(0,iIdx));
 		String strElementName(strElement.substr(iIdx+1));
 
-		IAS_LOG(true, strElement);
-
 		try{
-			IAS_LOG(true, strElementNS);
 			const DM::ComplexType* pRootType = pDataFactory->getType(strElementNS,DM::DataFactory::RootTypeName)->asComplexType();
-			IAS_LOG(true, strElementName);
 			pType = pRootType->getProperties().getProperty(strElementName)->getType();
 		}catch(...){
 			IAS_LOG(LogLevel::INSTANCE.isInfo(),CStrElementAttr<<" is useless.");
