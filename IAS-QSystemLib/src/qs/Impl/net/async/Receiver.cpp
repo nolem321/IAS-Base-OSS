3202c89162ceb38698ff10a89e9a8fedfe2e6045
InvenireAudeAdmin <albert.krzymowski@invenireaude.com>
2018-12-14 11:48:34 +0100

    http attribute REMOTE_ADDR

--- IAS-QSystemLib/src/qs/Impl/net/async/Receiver.cpp
+++ IAS-QSystemLib/src/qs/Impl/net/async/Receiver.cpp
@@ -1,14 +1,14 @@
 /*
  * File: IAS-QSystemLib/src/qs/Impl/net/async/Receiver.cpp
- * 
+ *
  * Copyright (C) 2015, Albert Krzymowski
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -61,7 +61,7 @@ API::Message* Receiver::receive(int iTimeWait, API::Attributes* pSelector){
 	StringStream ssValue;
 	ssValue<<"asrvhttp://localhost/"<<ptrConversation->getKey();
 	ptrMessage->getAttributes()->setReplyTo(ssValue.str());
-
+	ptrMessage->getAttributes()->setValue("REMOTE_ADDR",ptrConversation->getPeer().getAddress());
 	return ptrMessage.pass();
 }
 /*************************************************************************/
