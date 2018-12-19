9ddd83ae224442fde7bbcf4577dc7b5c22300f2b
InvenireAudeAdmin <albert.krzymowski@invenireaude.com>
2018-12-19 11:27:25 +0100

    standard imports reorganized

--- IAS-LangLib-TestCases/src/testcases/TCTypePrinting.cpp
+++ IAS-LangLib-TestCases/src/testcases/TCTypePrinting.cpp
@@ -1,14 +1,14 @@
 /*
  * File: IAS-LangLib-TestCases/src/testcases/TCTypePrinting.cpp
- * 
+ *
  * Copyright (C) 2015, Albert Krzymowski
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -33,8 +33,6 @@ using namespace Lang::Model;
 
 namespace TCT{
 
-//TODO  standarize !!!
-const String CEnv_SRC_DIRS="IAS_LANG_SRC_DIRS";
 
 /*************************************************************************/
 TCTypePrinting::TCTypePrinting(TestSuite* pTestSuite,
@@ -48,12 +46,10 @@ TCTypePrinting::TCTypePrinting(TestSuite* pTestSuite,
 
 	IAS_TRACER;
 	ptrModel = IAS_DFT_FACTORY<Lang::Model::Model>::Create();
-	StringList lstSrcPath;
-	IAS::EnvTools::GetEnvTokenized(CEnv_SRC_DIRS, lstSrcPath);
 
 	IAS_DFT_FACTORY<Tools::Parser::LexerIStreamFactoryForFiles>::PtrHolder ptrLexerIStreamFactory;
 	ptrLexerIStreamFactory = IAS_DFT_FACTORY<Tools::Parser::LexerIStreamFactoryForFiles>::Create();
-	ptrLexerIStreamFactory->setSearchPath(lstSrcPath);
+
 	ptrLexer=IAS_DFT_FACTORY<Script::Parser::Lexer>::Create<Tools::Parser::LexerIStreamFactory*,
 															Tools::Parser::ISourceLocationStore*>(
 																ptrLexerIStreamFactory.pass(),
@@ -103,7 +99,7 @@ String TCTypePrinting::getOutputFileName() {
 
 /*************************************************************************/
 void TCTypePrinting::caseOne(){
-	
+
 	IAS_LOG(IAS::LogLevel::INSTANCE.isInfo(),"Printing:"<<strProgramName);
 
 
